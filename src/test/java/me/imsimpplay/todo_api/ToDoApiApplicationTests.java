067ad5db6836ea1ffc3f45ae41f5dcc4d92bb226
Simpplay <Simpplay32@gmail.com>
2026-04-16 12:16:09 -0500

    Project creation

---
+++ src/test/java/me/imsimpplay/todo_api/ToDoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package me.imsimpplay.todo_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
